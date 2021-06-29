3c09dc8f46393daae9f32be2d7280ec01a8849bf
c.1 <1184849115@qq.com>
2021-06-29 21:13:00 +0800

    2

--- while 循环/while 循环/源.c
+++ while 循环/while 循环/源.c
@@ -1,63 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-
-//int main(){
-//	int a = 0;
-//	int b = 2;
-//	if (a==1)
-//	  if (b == 2)
-//		printf("hehe\n");
-//	  else 
-//		printf("hahah\n");
-//	return 0;
-//
-//
-//}
- ////////////两者做对比////////
-//int main()
-//{
-//	int a = 0; int b = 2; if (a == 1)
-//	{
-//		if (b == 2)
-//		{
-//			printf("hehe\n");
-//		}
-//	}
-//	else
-//	{
-//		printf("haha\n");
-//	}
-//	return 0;
-//}
-
-  ////需要注意 “=”赋值和 “==”等于的区别 常量赋值给变量 而变量不能赋值给常量
-////代码3
-//int num = 1; if (num == 5)
-//{
-//	printf("hehe\n");
-//}
-//
-////代码4
-//int num = 1; if (5 == num)
-//{ 
-//	printf("hehe\n");
-//}
-
-
-//int main()
-//{
-//	int i;
-//
-//	for (i = 0; i <= 100; i++)
-//	{
-//		if (1==i%2)
-//	
-//			printf("%d",i);
-//	}
-//	return 0;
-//}
-
 ///switch语句？//////////////
 
 //int main()
@@ -88,28 +28,28 @@
 //{
 //	int day=0;
 //	int n = 1;
-	/*scanf("%d", &day);
-	switch (day)
-	{
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-						printf("weekday\n");
-							break;
-	case  6:
-	case  7:
-									printf("weekend\n");
-									break;
-	default:
-		printf("输入错误\n");
+/*scanf("%d", &day);
+switch (day)
+{
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+printf("weekday\n");
+break;
+case  6:
+case  7:
+printf("weekend\n");
+break;
+default:
+printf("输入错误\n");
 
 */
 
- /*  }
+/*  }
 
-	return 0;
+return 0;
 }
 
 */
@@ -177,7 +117,6 @@ int main()
 		putchar(ch);
 	return 0;
 }       //ctrl+z结束循环
-
 
 
 
